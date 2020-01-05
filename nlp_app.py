bcd280eaf6c454f95aa0de1470d4ede654264c0b
Sudarshan <sudarshan85@gmail.com>
2020-01-05 14:51:21 -0500

    Displacy

--- nlp_app.py
+++ nlp_app.py
@@ -5,6 +5,7 @@ import streamlit as st
 from ttictoc import TicToc
 import numpy as np
 import spacy
+from spacy import displacy
 from textblob import TextBlob
 from gensim.summarization import summarize as gensim_summarizer
 
@@ -12,18 +13,20 @@ from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
 
+HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; border-radius: 0.25rem; padding: 1rem">{}</div>"""
+
 @st.cache(allow_output_mutation=True)
 def load_spacy(model='en_core_web_sm'):
   nlp = spacy.load(model)
   return nlp
 
-def text_analyzer(sp_model, text):
-  doc = sp_model(text)
-  return [(f"Token: {token.text}, Lemma: {token.lemma_}") for token in doc]
+@st.cache(allow_output_mutation=True, suppress_st_warning=True)
+def analyze_text(sp_model, text):
+  return sp_model(text)
 
-def entity_extractor(sp_model, text):
-  doc = sp_model(text)
-  return {ent.text: ent.label_ for ent in doc.ents}
+# def entity_extractor(sp_model, text):
+#   doc = sp_model(text)
+#   return {ent.text: ent.label_ for ent in doc.ents}
 
 def sumy_summarizer(text):
   parser = PlaintextParser.from_string(text, Tokenizer('english'))
@@ -34,7 +37,6 @@ def main():
   st.title("NLP App with Streamlit")
   nlp = load_spacy()
 
-  st.subheader("Basic NLP Tasks")
   text = st.text_area("Enter Text and select application from sidebar", "Type Here")
   apps = ['Show tokens & lemmas', 'Extract Entities', 'Show sentiment', 'Summarize text']
 
@@ -42,11 +44,14 @@ def main():
   if choice == "Show tokens & lemmas":
     if st.button("Tokenize"):
       st.info("Using spaCy for tokenization and lemmatization")
-      st.json(text_analyzer(nlp, text))
+      st.json([(f"Token: {token.text}, Lemma: {token.lemma_}") for token in analyze_text(nlp, text)])
   elif choice == 'Extract Entities':
     if st.button("Extract"):
-      st.json(entity_extractor(nlp, text))
-      st.info("Using spaCy for NER")
+      st.info("Using spaCy for NER")      
+      doc = analyze_text(nlp, text)
+      html = displacy.render(doc, style='ent')
+      html = html.replace('\n\n', '\n')
+      st.write(html, unsafe_allow_html=True)
   elif choice == "Show sentiment":
     if st.button("Analyze"):
       st.info("Using TextBlob for sentiment analysis")
@@ -58,13 +63,14 @@ def main():
       st.write(sentiment)
       st.info("Polarity is between -1 (negative) and 1 (positive) indicating the type of sentiment\nSubjectivity is between 0 (objective) and 1 (subjective) indicating the bias of the sentiment")
   elif choice == "Summarize text":    
-    summarizer_type = st.sidebar.selectbox("Select Summarizer", ['gensim', 'sumy'])
-    if summarizer_type == 'gensim':
+    summarizer_type = st.sidebar.selectbox("Select Summarizer", ['Gensim', 'Sumy Lex Rank'])
+    if summarizer_type == 'Gensim':
       summarizer = gensim_summarizer
-    elif summarizer_type == 'sumy':
+    elif summarizer_type == 'Sumy Lex Rank':
       summarizer = sumy_summarizer
 
     if st.button("Summarize"):
+      st.info(f"Using {summarizer_type} summarizer")
       st.success(summarizer(text))
   
 if __name__ == "__main__":
