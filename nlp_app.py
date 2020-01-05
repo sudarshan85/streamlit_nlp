a1ebf46c13708391c55c4a801f05f0340a82b066
Sudarshan <sudarshan85@gmail.com>
2020-01-05 13:09:07 -0500

    Refactor

--- nlp_app.py
+++ nlp_app.py
@@ -3,6 +3,7 @@
 import streamlit as st
 
 from ttictoc import TicToc
+import numpy as np
 import spacy
 from textblob import TextBlob
 from gensim.summarization import summarize as gensim_summarizer
@@ -34,38 +35,37 @@ def main():
   nlp = load_spacy()
 
   st.subheader("Basic NLP Tasks")
-  text = st.text_area("Enter Text and click on application", "Type Here")
-  
-  if st.button("Show Tokens & Lemmas"):
-    st.json(text_analyzer(nlp, text))
+  text = st.text_area("Enter Text and select application from sidebar", "Type Here")
+  apps = ['Show tokens & lemmas', 'Extract Entities', 'Show sentiment', 'Summarize text']
 
-  if st.button("Extract Entities"):
-    st.json(entity_extractor(nlp, text))
+  choice = st.sidebar.selectbox("Select Application", apps)
+  if choice == "Show tokens & lemmas":
+    if st.button("Tokenize"):
+      st.info("Using spaCy for tokenization and lemmatization")
+      st.json(text_analyzer(nlp, text))
+  elif choice == 'Extract Entities':
+    if st.button("Extract"):
+      st.json(entity_extractor(nlp, text))
+      st.info("Using spaCy for NER")
+  elif choice == "Show sentiment":
+    if st.button("Analyze"):
+      st.info("Using TextBlob for sentiment analysis")
+      blob = TextBlob(text)
+      sentiment = {
+        'polarity': np.round(blob.sentiment[0], 3),
+        'subjectivity': np.round(blob.sentiment[1], 3),
+      }
+      st.write(sentiment)
+      st.info("Polarity is between -1 (negative) and 1 (positive) indicating the type of sentiment\nSubjectivity is between 0 (objective) and 1 (subjective) indicating the bias of the sentiment")
+  elif choice == "Summarize text":    
+    summarizer_type = st.sidebar.selectbox("Select Summarizer", ['gensim', 'sumy'])
+    if summarizer_type == 'gensim':
+      summarizer = gensim_summarizer
+    elif summarizer_type == 'sumy':
+      summarizer = sumy_summarizer
 
-  if st.button("Show sentiment"):
-    blob = TextBlob(text)
-    st.write(blob.sentiment)
-    st.info("Polarity is between -1 (negative) and 1 (positive) indicating the type of sentiment\nSubjectivity is between 0 (objective) and 1 (subjective) indicating the bias of the sentiment")
-
-  st.subheader("Text Summarizer")
-  text = st.text_area("Enter Text", "Type Here")
-  
-  summerizer_type = st.selectbox("Summarizer", ("gensim", "sumy"))
-  if summerizer_type == 'gensim':
-    summarizer = gensim_summarizer
-    st.info("Using gensim summarizer...")
-  elif summerizer_type == 'sumy':
-    summarizer = sumy_summarizer
-    st.info("Using sumy summarizer...")
-
-  if st.button("Summarize"):
-    st.success(summarizer(text))
-
-  st.sidebar.subheader("App Details")
-  st.sidebar.text("Demo of using Streamlit to \ncreate basic NLP tasks")
-
-  if st.button("Thanks!"):
-    st.balloons()
+    if st.button("Summarize"):
+      st.success(summarizer(text))
   
 if __name__ == "__main__":
   main()
